272405be628a6cda6a3780e471027316896bd11d
Rodrigo Severo <rodrigoseverodev@gmail.com>
2024-09-17 14:35:40 -0400

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,225 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta name="description" content="" />
+    <meta name="author" content="" />
+    <title>Ciência e Tecnologia</title>
+    <!-- Favicon-->
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+    <!-- Font Awesome icons (free version)-->
+    <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+    <!-- Google fonts-->
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet"
+        type="text/css" />
+    <!-- Core theme CSS (includes Bootstrap)-->
+    <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body id="page-top">
+    <!-- Navigation-->
+    <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand" href="#page-top">Ciência e Tecnologia</a>
+            <button class="navbar-toggler text-uppercase font-weight-bold bg-primary text-white rounded" type="button"
+                data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
+                            href="#sistemas">Sistemas</a></li>
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
+                            href="#about">Sobre</a></li>
+                    <!-- <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded"
+                            href="#contact">Contact</a></li> -->
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!-- Masthead-->
+
+    <!-- Sistemas Section-->
+    <section class="page-section portfolio mt-5" id="sistemas">
+        <div class="container">
+            <!-- Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Acesso aos sistemas</h2>
+            <!-- Icon Divider-->
+            <div class="divider-custom">
+                <div class="divider-custom-line"></div>
+            </div>
+
+            <!-- Cards for Nuvem -->
+
+            <div class="row">
+               
+                <!-- <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 15</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio15" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 18</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio18" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 19</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio19" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 2020</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio2020" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 2021</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio21" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 2022</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio22" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div> -->
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Convenio 2024</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/convenio2024" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Seleção Bolsista</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/uab/selecaobolsauab/" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">PERS MT</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/persmt" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">TCE - Portal</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/portal/" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+
+                
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">AVA TCE</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/ava-tce" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Card 2 -->
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Cursos EAD</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/cursosead" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+                <!-- Add more cards similarly -->
+                <div class="col-md-6 col-lg-3 mb-5">
+                    <div class="card h-100">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">MBA Gestão Estratégica</h5>
+                            <a target="_blank" href="https://setec.ufmt.br/tce/mba-gestrategica" class="btn btn-primary">Acessar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </section>
+    <!-- About Section-->
+    <section class="page-section bg-primary text-white mb-0" id="about">
+        <div class="container">
+            <!-- About Section Heading-->
+            <h2 class="page-section-heading text-center text-uppercase text-white">Sobre</h2>
+            <!-- Icon Divider-->
+            <div class="divider-custom divider-light">
+                <div class="divider-custom-line"></div>
+                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                <div class="divider-custom-line"></div>
+            </div>
+            <!-- About Section Content-->
+            <div class="row">
+                <div class="col-lg-12 ms-auto">
+                    <p class="lead">
+                        Aliquam tincidunt elementum urna, non aliquet leo laoreet et. Etiam at orci sem. Nunc malesuada eu neque porta dapibus. Mauris laoreet sed tellus sit amet pharetra. Nam ut massa elit. Nulla bibendum accumsan nulla, nec sagittis augue convallis in. Curabitur blandit risus non porta suscipit. Mauris non lacinia mauris, ac sagittis sem. Curabitur at nibh sed nisl faucibus pulvinar et vel tellus. Suspendisse facilisis mi id est lacinia viverra. In felis neque, iaculis ut fermentum vitae, tristique sed ante. Phasellus eu ligula ut nisi gravida porta sit amet et leo. Sed sodales finibus leo, id tristique neque pretium et.
+                    </p>
+                </div>
+
+            </div>
+            <!-- About Section Button-->
+            <!-- <div class="text-center mt-4">
+                    <a class="btn btn-xl btn-outline-light" href="https://startbootstrap.com/theme/freelancer/">
+                        <i class="fas fa-download me-2"></i>
+                        Free Download!
+                    </a>
+                </div> -->
+        </div>
+    </section>
+    <!-- Contact Section-->
+
+    <!-- Footer-->
+
+    <!-- Copyright Section-->
+    <div class="copyright py-4 text-center text-white">
+        <div class="container"><small>&copy; Ciência e Tecnologia - <?= date("Y"); ?></small></div>
+    </div>
+
+    <!-- Bootstrap core JS-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- Core theme JS-->
+    <script src="js/scripts.js"></script>
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <!-- * *                               SB Forms JS                               * *-->
+    <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+    <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+    <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+</body>
+
+</html>
